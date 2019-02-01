8766bc7b5bbe0176501f39c961a3415e13496bb6
Ross Rosen <rrosen326@gmail.com>
2019-02-01 05:32:48 -0800

    Fixed but - will not fail on bad log lines

--- Development.md
+++ Development.md
@@ -1,7 +1,13 @@
 # Development Page
 
 ## TODO
-* P1
+* Export button
+* Bug
+    * When you enter data on the phone, it should show up on the desktop
+    * Would need my own web socket - maybe too hard
+* UI - date limit?
+* Command line
+    * Parameter to limit by date
 
 
 * Bundle size
@@ -15,3 +21,21 @@
     * Setting IPs
     * Navigation guards
  
+
+## Github release process
+A simplified version of [this](https://www.braintreepayments.com/blog/our-git-workflow/)  
+
+
+Note - this is a little confusing because I used "github" both as a release and as a branch.  
+
+Also, they have an extra branch in the middle (release) that I don't have.
+```
+git checkout github
+git merge --squash master
+git commit -m 'Info about release'
+git tag -a 1.0.0 -m '1.0.0'
+git push github HEAD:master
+git checkout master
+git merge github
+git push origin master
+```
--- python_app/lib/parse_hubitat_log.py
+++ python_app/lib/parse_hubitat_log.py
@@ -1,5 +1,6 @@
 import re
 from pathlib import Path
+from datetime import datetime as dt
 
 SAMPLE_DATA=["2018-11-26 00:35:16.378\tWARN \tdev|166|Sonos - Bedroom 2 |Connection refused (Connection refused)"
 "2018-11-26 00:35:16.424\tWARN \tdev|166|Sonos - Bedroom 2 |Connection refused (Connection refused)"]
@@ -14,21 +15,40 @@ LEVEL_INT = {
 
 def parse_log_line(line):
     linemod = line.encode('utf-8').decode('unicode-escape')
-    if linemod[-1] == '\n':
-        linemod = linemod[0:-1]
-    if linemod[0] == '"' and linemod[-1] == '"':
-        linemod = linemod[1:-1]
-    fields = linemod.split('\t')
-    fields2 = fields[2].split('|')
-    data = {
-        'time': fields[0].strip(),
-        'level': fields[1].strip().lower(),
-        'type': fields2[0].strip(),
-        'id': fields2[1].strip(),
-        'name': fields2[2].strip(),
-        'msg': fields2[3].strip()
-    }
-    data['levelInt'] = LEVEL_INT[data['level']]
+    try:
+        if linemod[-1] == '\n':
+            linemod = linemod[0:-1]
+        if linemod[0] == '"' and linemod[-1] == '"':
+            linemod = linemod[1:-1]
+        fields = linemod.split('\t')
+        fields2 = fields[2].split('|')
+        data = {
+            'time': fields[0].strip(),
+            'level': fields[1].strip().lower(),
+            'type': fields2[0].strip(),
+            'id': fields2[1].strip(),
+            'name': fields2[2].strip(),
+            'msg': fields2[3].strip()
+        }
+        data['levelInt'] = LEVEL_INT[data['level']]
+    except Exception as err:
+        """
+        Some lines don't parse correctly. For instance, the following is in the logs - and it is one
+        log item but 4 lines. Not worth fixing.
+        
+        dev:1642019-01-31 09:47:08.483 am errororg.codehaus.groovy.runtime.metaclass.MethodSelectionException: Could not find which method playTrack() to invoke from this list:
+        public java.lang.Object sonosDriver#playTrack(java.lang.String)
+        public java.lang.Object sonosDriver#playTrack(java.util.Map)
+        public java.lang.Object sonosDriver#playTrack(java.lang.String, java.lang.Object) (playTrack)
+        """
+        data = {
+            'time': dt.now().isoformat(' '),
+            'level': "error",
+            'type': "error",
+            'id': "error",
+            'name': '_error: Can not parse',
+            'msg': linemod,
+        }
 
     return data
 
